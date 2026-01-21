1e9d4f42b88cfa80f6642f55caec3be36c143877
misterveyda <veydaszn@gmail.com>
2026-01-21 19:44:30 +0300

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot_topic_distribution(model):
+    plt.bar(range(len(model.components_)), 
+            [sum(topic) for topic in model.components_])
+    plt.xlabel("Topics")
+    plt.ylabel("Weight")
+    plt.title("Topic Distribution")
+    plt.show()
 
