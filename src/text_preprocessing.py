7c5bdbca8b90b896d8a042e01e57ad8058514b2e
misterveyda <veydaszn@gmail.com>
2026-01-21 19:42:50 +0300

    Update text_preprocessing.py

--- src/text_preprocessing.py
+++ src/text_preprocessing.py
@@ -1,1 +1,13 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    tokens = text.split()
+    tokens = [t for t in tokens if t not in stopwords.words('english')]
+    return " ".join(tokens)
 
